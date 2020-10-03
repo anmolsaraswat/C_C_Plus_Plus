b49cc844edfecd0462debd66734baf8b2eafee5d
Anmol Saraswat <anmolsaraswat@outlook.com>
2020-10-03 13:01:15 +0530

    insertion in sets

---
+++ Standard Template Library/Sets/insertion_in_sets.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <set>
+#include <string>
+#include <functional>
+
+using namespace std;
+
+int main()
+{
+    set <int> s1;
+    s1.insert(10);
+    s1.insert(60);
+    s1.insert(22);
+    s1.insert(40);
+
+    set <int>:: iterator itr;
+    for (itr = s1.begin(); itr!=s1.end(); itr++)
+    {
+        cout << *itr << " ";
+    }
+    return 0;
+}
